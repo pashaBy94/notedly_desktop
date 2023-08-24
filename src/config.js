c6617ee806b8915da2d6b34e4d046d3471e59e82
pashaBy94 <pascom_by@mail.ru>
2023-08-24 23:47:34 +0300

    desktop_icons2

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  LOCAL_WEB_URL: 'http://localhost:1234/',
+  PRODUCTION_WEB_URL: 'https://delicate-rolypoly-1439c6.netlify.app',
+  PRODUCTION_API_URL: 'https://jslistnote-367662497dfd.herokuapp.com/api'
+};
+
+module.exports = config;
